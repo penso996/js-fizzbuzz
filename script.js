3774fda61cc5c937221d2a97fa8e7afdd5263c9e
penso996 <penso.andrea@outlook.it>
2024-12-18 15:50:23 +0100

    first version - not working duplicate problem

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 //Partire chiedendo al computer di stampare dei numeri da 1 a 100.
 for (let i = 1; i <= 100; i++) {
-    console.log(i);
+
     //Mentre il computer elabora 1 ad 1 l'output verificare man mano risultato per risultato.
     //Quando un numero è:
     //MULTIPLO DI 3: Stampare Fizz al posto del numero di output.
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    }
 
     //MULTIPLO DI 5: Stampare Buzz al posto del numero di output.
+    if (i % 5 === 0) {
+        console.log("Buzz")
+    }
 
     //MULTIPLO DI 3 E 5: Stampare FizzBuzz al posto del numero di output.
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
 
     //ALTRIMENTI: Stampare il numero.
-
+    else {
+        console.log(i);
     }
+
+}
